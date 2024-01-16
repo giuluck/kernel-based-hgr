47971ee3f8b8dcabc09e16211dcbbb83b28b26ad
Luca Giuliani <luca.giuliani10@studio.unibo.it>
2024-01-16 17:34:30 +0100

    Implement mlp model and add default metrics for correlation

---
+++ src/metrics/kde.py
@@ -0,0 +1,68 @@
+from dataclasses import dataclass, field
+from math import sqrt, pi
+
+import torch
+
+from src.metrics.base import CorrelationMetric
+
+
+class KDE:
+    """A Gaussian KDE implemented in pytorch for the gradients to flow in pytorch optimization."""
+
+    SQRT_PI: float = sqrt(2 * pi)
+
+    def __init__(self, train):
+        n, d = train.shape
+        self.dimension = d
+        self.bandwidth = (n * (d + 2) / 4.) ** (-1. / (d + 4))
+        self.train = train
+
+    @staticmethod
+    def _unsqueeze_multiple_times(inp, axis, times):
+        out = inp
+        for i in range(times):
+            out = out.unsqueeze(axis)
+        return out
+
+    def pdf(self, x):
+        assert x.shape[-1] == self.dimension, f"Expected dimension {self.dimension}, got {x.shape[-1]}"
+        data = x.unsqueeze(-2)
+        train = self._unsqueeze_multiple_times(self.train, 0, len(x.shape) - 1)
+        # noinspection PyTypeChecker
+        gaussian_values = torch.exp(-((data - train).norm(dim=-1) ** 2 / (self.bandwidth ** 2) / 2))
+        return gaussian_values.mean(dim=-1) / self.bandwidth / self.SQRT_PI
+
+
+@dataclass(frozen=True)
+class DensityHGR(CorrelationMetric):
+    """Torch-based implementation of the HGR/CHI2 metric obtained from the official repository of "Fairness-Aware
+    Learning for Continuous Attributes and Treatments" (https://github.com/criteo-research/continuous-fairness/)."""
+
+    chi2: bool = field()
+    """Whether to return the chi^2 approximation of the HGR or its actual value."""
+
+    @staticmethod
+    def joint_2(x: torch.Tensor, y: torch.Tensor, damping: float = 1e-10, eps: float = 1e-9) -> torch.Tensor:
+        # add an eps value to avoid nan vectors in case of very degraded solutions
+        x = (x - x.mean()) / (x.std(dim=None) + eps)
+        y = (y - y.mean()) / (y.std(dim=None) + eps)
+        data = torch.cat([x.unsqueeze(-1), y.unsqueeze(-1)], -1)
+        joint_density = KDE(data)
+        n_bins = int(min(50, 5. / joint_density.bandwidth))
+        x_centers = torch.linspace(-2.5, 2.5, n_bins)
+        y_centers = torch.linspace(-2.5, 2.5, n_bins)
+        xx, yy = torch.meshgrid([x_centers, y_centers], indexing='ij')
+        grid = torch.cat([xx.unsqueeze(-1), yy.unsqueeze(-1)], -1)
+        h2d = joint_density.pdf(grid) + damping
+        h2d /= h2d.sum()
+        return h2d
+
+    def correlation(self, a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
+        h2d = DensityHGR.joint_2(a, b)
+        marginal_a = h2d.sum(dim=1).unsqueeze(1)
+        marginal_b = h2d.sum(dim=0).unsqueeze(0)
+        q = h2d / (torch.sqrt(marginal_a) * torch.sqrt(marginal_b))
+        if self.chi2:
+            return (q ** 2).sum(dim=[0, 1]) - 1.0
+        else:
+            return torch.linalg.svd(q)[1][1]
---
+++ src/model.py
@@ -0,0 +1,143 @@
+from typing import Optional, Union, Iterable, Tuple, Dict
+
+import lightning.pytorch as pl
+import numpy as np
+import torch
+from torch import nn, Tensor
+from torch.autograd import Variable
+from torch.optim import Optimizer, Adam
+from torch.utils.data import Dataset
+
+from src.metrics.base import CorrelationMetric
+
+
+class Data(Dataset):
+    """Default dataset for Torch."""
+
+    def __init__(self, x, y):
+        assert len(x) == len(y), f"Data should have the same length, but len(x) = {len(x)} and len(y) = {len(y)}"
+        self.x = torch.tensor(np.array(x), dtype=torch.float32)
+        self.y = torch.tensor(np.expand_dims(y, axis=-1), dtype=torch.float32)
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, idx):
+        return self.x[idx], self.y[idx]
+
+
+class MultiLayerPerceptron(pl.LightningModule):
+    """Template class for a Multi-layer Perceptron in Pytorch Lightning."""
+
+    THRESHOLD: float = 0.2
+    """The threshold to be imposed in the penalty."""
+
+    def __init__(self,
+                 units: Iterable[int],
+                 features: Iterable[str],
+                 classification: bool,
+                 penalty: Optional[CorrelationMetric] = None,
+                 alpha: Optional[float] = None):
+        """
+        :param units:
+            The neural network hidden units.
+
+        :param features:
+            The neural network input features.
+
+        :param classification:
+            Whether we are dealing with a binary classification or a regression task.
+
+        :param penalty:
+            The kind of penalty to be imposed, or None for unconstrained model.
+
+        :param alpha:
+            The weight of the penalizer, or None for automatic weight regularization via lagrangian dual technique.
+            If the penalty is None, must be None as well and it is ignored.
+        """
+        super(MultiLayerPerceptron, self).__init__()
+
+        # disable lightning manual optimization to potentially deal with two optimizers
+        self.automatic_optimization = False
+
+        # build the layers by prepending the initial and final units
+        layers = []
+        features = list(features)
+        units = [len(features), *units]
+        for inp, out in zip(units[:-1], units[1:]):
+            layers.append(nn.Linear(inp, out))
+            layers.append(nn.ReLU())
+        layers.append(nn.Linear(units[-1], 1))
+
+        # if there is a penalty and alpha is None, then build a variable for alpha
+        if alpha is None and penalty is not None:
+            alpha = Variable(torch.zeros(1), requires_grad=True, name='alpha')
+        # otherwise, check that either there is a penalty or alpha is None (since there is no penalty)
+        else:
+            assert penalty is not None or alpha is None, "If penalty=None, alpha must be None as well."
+
+        self.penalty: CorrelationMetric = penalty
+        """The kind of penalty to be imposed, or None for unconstrained model."""
+
+        self.alpha: Union[None, float, Variable] = alpha
+        """The alpha value for balancing compiled and regularized loss."""
+
+        self.loss: nn.Module = nn.BCELoss() if classification else nn.MSELoss()
+        """The loss function."""
+
+        self.model = nn.Sequential(*layers)
+        """The neural network."""
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Performs the forward pass on the model given the input (x)."""
+        return self.model(x)
+
+    def training_step(self, batch: Tuple[Tensor, Tensor], batch_idx: int) -> Dict[str, Tensor]:
+        """Performs a training step on the given batch."""
+        # retrieve the data and the optimizers
+        inp, out = batch
+        optimizers = self.optimizers()
+        def_opt, reg_opt = optimizers if isinstance(optimizers, tuple) else (optimizers, None)
+        # perform the standard loss minimization step
+        def_opt.zero_grad()
+        pred = self.model(inp)
+        def_loss = self.loss(pred, out)
+        # if there is a penalty, compute it for the minimization step
+        if self.penalty is None:
+            reg = torch.tensor(0.0)
+            alpha = torch.tensor(0.0)
+            reg_loss = torch.tensor(0.0)
+        else:
+            reg = self.correlation(a=inp[:, self.penalty.feature], b=pred.squeeze())
+            reg_loss = self.alpha * reg
+            alpha = torch.tensor(self.alpha)
+        # build the total minimization loss and perform the backward pass
+        tot_loss = def_loss + reg_loss
+        self.manual_backward(tot_loss)
+        def_opt.step()
+        # if there is a variable alpha, perform the maximization step (loss + penalty with switched sign)
+        if isinstance(self.alpha, Variable):
+            reg_opt.zero_grad()
+            pred = self.model(inp)
+            def_loss = self.loss(pred, out)
+            reg = self.correlation(a=inp[:, self.penalty.feature], b=pred.squeeze())
+            reg_loss = self.alpha * reg
+            tot_loss = def_loss + reg_loss
+            self.manual_backward(-tot_loss)
+            reg_opt.step()
+        # return the information about the training
+        return {
+            'pred': pred,
+            'loss': tot_loss,
+            'def_loss': def_loss,
+            'reg_loss': reg_loss,
+            'alpha': alpha,
+            'reg': reg
+        }
+
+    def configure_optimizers(self) -> Union[Optimizer, Tuple[Optimizer, Optimizer]]:
+        """Configures the optimizer for the MLP depending on whether there is a variable alpha or not."""
+        if isinstance(self.alpha, Variable):
+            return Adam(params=self.model.parameters()), Adam(params=[self.alpha])
+        else:
+            return Adam(params=self.model.parameters())
